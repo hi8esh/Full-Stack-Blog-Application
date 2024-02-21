62abf8900b814918bf42edff15eb963f7d644f6a
hi8esh <maheshsudha56@gmail.com>
2024-02-21 12:21:01 +0530

    Added Commenting functionality

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -3,14 +3,17 @@ import express from 'express';
 let articlesInfo = [{
                     name: 'learn-react',
                     upvotes: 0,
+                    comments: [],
                 },
                 {
                     name: 'learn-node',
                     upvotes: 0,
+                    comments: [],
                 },
                 {
                     name: 'mongodb',
                     upvotes: 0,
+                    comments: [],
                 }]
 
 const app = express();
@@ -38,9 +41,25 @@ app.use(express.json()); // for reading json added in body of post call.
 app.put('/api/articles/:name/upvote', (req, res) => {
     const { name } = req.params;
     const article = articlesInfo.find(a => a.name === name);
+    
     if(article){
         article.upvotes += 1;
         res.send(`The ${name} article now has ${article.upvotes} upvotes!!!`);
+    }
+    else{
+        res.send('That article doesn\'t exist');
+    }
+
+});
+
+app.post('/api/articles/:name/comments', (req, res) => {
+    const { name } = req.params;
+    const { postedBy, text } = req.body;
+    const article = articlesInfo.find(a => a.name === name);
+    
+    if(article){
+        article.comments.push({postedBy, text});
+        res.send(article.comments);
     }
     else{
         res.send('That article doesn\'t exist');
