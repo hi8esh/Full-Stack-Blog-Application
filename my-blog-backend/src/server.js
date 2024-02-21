3a3f534848ecaa6c7f0cdbba5338ac89f239e441
hi8esh <maheshsudha56@gmail.com>
2024-02-21 10:59:06 +0530

    URL prams

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -4,13 +4,22 @@ const app = express();
 
 app.use(express.json()); // for reading json added in body of post call.
 
-app.get('/hello', (req, res) => {
-    res.send('Hello! Get');
-});
-
 app.post('/hello', (req, res) => {
     console.log(req.body);
     res.send(`Hello ${req.body.name}!`);
+});
+
+app.get('/hello/:name', (req, res) => {
+    //const name = req.params.name;
+    const { name } = req.params;
+    res.send(`Hello ${name}!!`);
+});
+
+app.get('/hello/:name/goodbye/:otherName', (req, res) => {
+    //const name = req.params.name;
+    console.log(req.params);
+    const { name } = req.params;
+    res.send(`Hello ${name}!!`);
 });
 
 app.listen(8000, () => {
